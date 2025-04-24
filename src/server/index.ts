10d22d6d0ac1d3e1d89d9a1c0d47fc8c2f90dfc3
mayority <mayority11@gmail.com>
2025-04-24 14:25:10 +0100

    Update API base URL to production; comment out local development URL in index.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import LocalStorage, { STORAGE_KEY } from "@/lib/storage";
 
-// const BASEURL = "https://api.gospofy.com/api";
-const BASEURL = "http://localhost:7890/api";
+const BASEURL = "https://api.gospofy.com/api";
+// const BASEURL = "http://localhost:7890/api";
 
 export enum REQUEST_TYPE {
   GET = "GET",
